70b711e461c8daf50b5815e2066ebfec8201910a
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-04 17:24:51 +0200

    initial quaries

---
+++ find_device_id_lookup.js
@@ -0,0 +1,24 @@
+db.server.aggregate([
+    { $match: {_id:1178511} },
+    {
+        $lookup:
+            {
+                from: "site",
+                localField: "siteid",
+                foreignField: "_id",
+                as: "site"
+            }    
+    },
+    {
+        $unwind: {path: "$site", preserveNullAndEmptyArrays: true}
+    },
+    {
+         $lookup:
+            {
+                from: "client",
+                localField: "site.clientid",
+                foreignField: "_id",
+                as: "client"
+            }      
+    },
+])
