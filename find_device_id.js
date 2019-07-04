e1ce3ca519c4183dd1fac668f0d0ba6d0ccf6f10
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-04 16:53:04 +0200

    initial quaries

---
+++ find_device_id.js
@@ -0,0 +1,15 @@
+db.server.aggregate([
+    { $match: {_id:1178511} },
+    {
+        $lookup:
+            {
+                from: "site",
+                localField: "siteid",
+                foreignField: "_id",
+                as: "site_doc"
+            }    
+    },
+    {
+        $unwind
+    }
+])
