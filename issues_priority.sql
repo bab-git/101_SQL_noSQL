8fdef3bbc8f708c6056e98eaf4f9962bd1bc35a6
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-04 15:18:14 +0200

    initializing the git

--- issues_priority.sql
+++ issues_priority.sql
@@ -5,10 +5,10 @@ FROM
 	101redmine.issues
 -- where 
 where 
-	created_on >= "2019-06-01"
+	created_on >= "2019-07-01"
     -- between "2014-06-01" and "2014-07-01"
 -- 	priority_id = 2
     -- project_id = 4
 -- LIMIT 100
 group by
-	priority_idcd 
+	priority_id 
