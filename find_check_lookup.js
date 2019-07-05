829c9263f110e3e452d921e9610f3fd23044655a
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-05 08:57:58 +0200

    check lookup table

---
+++ find_check_lookup.js
@@ -0,0 +1,41 @@
+// db.workstation.aggregate([
+//     { $match: {_id:1096537}},    
+//     { $match: {_id:1154763}},        
+db.server.aggregate([
+//     { $match: {_id:1060893} },            
+    {
+        $lookup:
+            {
+                from: "site",
+                localField: "siteid",
+                foreignField: "_id",
+                as: "site"
+            }    
+    },
+    {
+        $unwind: {path: "$site", preserveNullAndEmptyArrays: true}
+    },
+    {
+         $lookup: 
+            {
+                from: "client",
+                localField: "site.clientid",
+                foreignField: "_id",
+                as: "client"
+            }      
+    },
+    {
+        $unwind: {path: "$client", preserveNullAndEmptyArrays: true}
+    },
+    {
+            "$project": {
+                "_class":0,
+                "guid":0,
+                "lastBootTime":0,
+                "utcOffset":0,
+                "agentVersion":0,                               
+                }
+    },
+    { $match: {"client.apiKey":"ae0a4c75230afae756fcfecd3d2838cf"}},
+    { $match: {"site.enabled":true}}
+])
