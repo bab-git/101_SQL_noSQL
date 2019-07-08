7f4448376c18dee29bf3734a3d3db3a9335c9cdc
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-08 09:35:19 +0200

    test aggregate

--- find_check_lookup.js
+++ find_check_lookup.js
@@ -1,41 +1,80 @@
-// db.workstation.aggregate([
-//     { $match: {_id:1096537}},    
-//     { $match: {_id:1154763}},        
-db.server.aggregate([
-//     { $match: {_id:1060893} },            
-    {
-        $lookup:
-            {
-                from: "site",
-                localField: "siteid",
-                foreignField: "_id",
-                as: "site"
-            }    
-    },
-    {
-        $unwind: {path: "$site", preserveNullAndEmptyArrays: true}
-    },
-    {
-         $lookup: 
-            {
-                from: "client",
-                localField: "site.clientid",
-                foreignField: "_id",
-                as: "client"
-            }      
-    },
-    {
-        $unwind: {path: "$client", preserveNullAndEmptyArrays: true}
-    },
-    {
-            "$project": {
-                "_class":0,
-                "guid":0,
-                "lastBootTime":0,
-                "utcOffset":0,
-                "agentVersion":0,                               
-                }
-    },
-    { $match: {"client.apiKey":"ae0a4c75230afae756fcfecd3d2838cf"}},
-    { $match: {"site.enabled":true}}
+db.check.aggregate([
+//      { $match: {_id:ObjectId("5c1bbcfbfe78c90007af2693")} },            
+    { $match:
+        {
+            datetime: 
+            { "$gt" : 
+                new Date("2019-07-01")            
+            }        
+        }
+    },
+    {
+        $lookup:
+            {
+                from: "workstation",
+                localField: "deviceid",
+                foreignField: "_id",
+                as: "workstation"
+            }    
+    },
+    {
+        $unwind: {path: "$workstation", preserveNullAndEmptyArrays: true}
+    },
+    {
+        $lookup:
+            {
+                from: "server",
+                localField: "deviceid",
+                foreignField: "_id",
+                as: "server"
+            }    
+    },   
+    
+    {
+        $unwind: {path: "$server", preserveNullAndEmptyArrays: true}
+    },
+    {
+        $lookup:
+            {
+                from: "site",
+                localField: "server.siteid",
+                foreignField: "_id",
+                as: "site"
+            }    
+    },    
+    {
+        $lookup:
+            {
+                from: "site",
+                localField: "workstation.siteid",
+                foreignField: "_id",
+                as: "site"
+            }    
+    },           
+//     {
+//         $unwind: {path: "$site", preserveNullAndEmptyArrays: true}
+//     },
+//     {
+//          $lookup: 
+//             {
+//                 from: "client",
+//                 localField: "site.clientid",
+//                 foreignField: "_id",
+//                 as: "client"
+//             }      
+//     },
+//     {
+//         $unwind: {path: "$client", preserveNullAndEmptyArrays: true}
+//     },
+//     {
+//             "$project": {
+//                 "_class":0,
+//                 "guid":0,
+//                 "lastBootTime":0,
+//                 "utcOffset":0,
+//                 "agentVersion":0,                               
+//                 }
+//     },
+//     { $match: {"client.apiKey":"ae0a4c75230afae756fcfecd3d2838cf"}},
+//     { $match: {"site.enabled":true}}
 ])
