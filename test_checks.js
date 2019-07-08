1c51ac97f753749b1a16724310673022802b91b9
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-08 16:56:26 +0200

    check lookup works, but slow!

--- test_checks.js
+++ test_checks.js
@@ -1,9 +1,22 @@
-db.check.find(
-    {
-        datetime: 
-        { "$gt" : 
-//             new Date("2019-02-01")
-            new Date("2019-07-01")            
+// DBQuery.shellBatchSize = 500;
+db.getCollection('check').find(
+//     {     
+//         $match:
+        {
+            $and:
+            [
+//                 {datetime: { "$gt" : new ISODate("2019-02-04 01:00:10.000Z")}},
+//                 {datetime: { "$lt" : new ISODate("2019-07-01 01:00:17")}}
+               {datetime: { "$gt" : new ISODate("2019-02-04 10:48:00")}},
+               {datetime: { "$lt" : new ISODate("2019-02-04 18:50:00")}},
+//                    {datetime: { "$gt" : new Date("2018-12-03")}},
+//                 {datetime: { "$lt" : Date("2019-02-05")}},
+//                 {datetime: new ISODate("2019-07-01 01:00:17.000Z")},
+//                 {emailalerts: 1}
+//                 {checkid: {$in: ["23538852","22261308"]}}
+//                {description : /Festplattenspei/},
+               {checkstatus : "testerror"}
+            ]                         
         }        
-    }
-)
+//     }
+    ).count()
