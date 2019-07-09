c298c4be95b550ac93eb00d5f70bd7497f3b3bcf
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-09 10:35:09 +0200

    redmine DB

--- issues_J_clients.sql
+++ issues_J_clients.sql
@@ -1,8 +1,8 @@
-SELECT *,
-	prj.name
+SELECT *
+	-- prj.name
 FROM 101redmine.issues as iss
 Join 101redmine.projects as prj on iss.priority_id = prj.id
 where 
-	iss.created_on >= "2019-07-01"
+	-- iss.created_on >= "2019-07-01"
 	-- and priority_id = 3
-    -- iss.id = 52941 
+    iss.id in (52945,52956,52968)
