7a1d34bbfa8e219a14443895762869c5614ab943
Babak Hosseini <bbkhosseini@gmail.com>
2019-07-09 15:40:31 +0200

    mongoDB checks

--- test_checks.js
+++ test_checks.js
@@ -1,22 +1,35 @@
 // DBQuery.shellBatchSize = 500;
-db.getCollection('check').find(
+ DBQuery.shellBatchSize = 50;
+db.check.find(
 //     {     
 //         $match:
         {
-            $and:
-            [
+//             $and:
+//             [
 //                 {datetime: { "$gt" : new ISODate("2019-02-04 01:00:10.000Z")}},
-//                 {datetime: { "$lt" : new ISODate("2019-07-01 01:00:17")}}
-               {datetime: { "$gt" : new ISODate("2019-02-04 10:48:00")}},
-               {datetime: { "$lt" : new ISODate("2019-02-04 18:50:00")}},
+//                 {servertime: { "$eq" : ISODate("2019-07-09 13:09:17")}}
+//                datetime: { "$gt" : ISODate("2019-07-04 10:48:00")}
+               deviceid: {"$gt":267576}
+//                servertime: new ISODate("2018-12-21 23:17:30.000Z")
+//                datetime: new ISODate("2018-07-11 10:32:54.000Z")
+//                 {datetime: { "$lt" : new ISODate("2019-07-09 03:00:00")}}
 //                    {datetime: { "$gt" : new Date("2018-12-03")}},
 //                 {datetime: { "$lt" : Date("2019-02-05")}},
-//                 {datetime: new ISODate("2019-07-01 01:00:17.000Z")},
+//                 datetime: new ISODate("2020-01-01")
 //                 {emailalerts: 1}
 //                 {checkid: {$in: ["23538852","22261308"]}}
 //                {description : /Festplattenspei/},
-               {checkstatus : "testerror"}
-            ]                         
+//                {checkstatus : "testerror"}
+//             ]                         
         }        
+//         ,{
+//             datetime :1,
+//             servertime :1
+//         }
 //     }
-    ).count()
+    ).limit(50)
+     .sort({
+            "deviceid" : 1,
+            "description" : 1
+            }
+           )
