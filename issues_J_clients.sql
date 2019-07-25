d9b0485e1a882767f241015f205e8ce1742e3989
Babak Hosseini <Babak Hosseini@zentrale.de>
2019-07-25 16:35:14 +0200

    SQL vs redmine

--- issues_J_clients.sql
+++ issues_J_clients.sql
@@ -1,8 +1,9 @@
 SELECT *
 	-- prj.name
 FROM 101redmine.issues as iss
-Join 101redmine.projects as prj on iss.priority_id = prj.id
+Join 101redmine.projects as prj on iss.project_id = prj.id
 where 
 	-- iss.created_on >= "2019-07-01"
 	-- and priority_id = 3
-    iss.id in (52945,52956,52968)
+    iss.id in (52494,52329,52219,53634,52945,52956,52968)
+    -- iss.id > 52215 and iss.id < 52230
