7d71c34e7ac195a22cebe28d19c1eaefe1f75efe
Babak Hosseini <Babak Hosseini@zentrale.de>
2019-07-25 18:05:01 +0200

    mongoDB: follow consqtive fails using servertime+deviceid

--- check_server_time.js
+++ check_server_time.js
@@ -3,10 +3,11 @@ db.getCollection('check').find(
         $and: 
         [   
 //             {servertime: new ISODate("2019-07-20 12:02:34.000Z")},
-            {servertime: {"$gt": new ISODate("2019-07-20 03:00:00")}},
-            {servertime: {"$lt": new ISODate("2019-07-20 16:00:00")}},
-            {checkstatus: {$ne:"testok"}},
-            {deviceid:1145595}
+            {servertime: {"$gt": new ISODate("2019-07-20 00:00:00")}},
+            {servertime: {"$lt": new ISODate("2019-07-20 23:00:00")}},
+            {checkstatus: {$ne:"testok"}},
+//             {checkid:"20126304"},
+            {deviceid:563882}
         ]
     }        
-).limit(30)
+).limit(100)
