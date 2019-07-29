da1df9102ab25a3b7caac7d17779e25c52a7831e
Babak Hosseini <Babak Hosseini@zentrale.de>
2019-07-29 17:55:39 +0200

    mongoDB: work with servertime

--- check_server_time.js
+++ check_server_time.js
@@ -1,13 +1,19 @@
 db.getCollection('check').find(
     {
-        $and: 
-        [   
+         $and: 
+         [   
 //             {servertime: new ISODate("2019-07-20 12:02:34.000Z")},
-            {servertime: {"$gt": new ISODate("2019-07-20 00:00:00")}},
-            {servertime: {"$lt": new ISODate("2019-07-20 23:00:00")}},
+            {servertime: {"$gt": new ISODate("2019-07-01 00:00:00")}},
+            {servertime: {"$lt": new ISODate("2019-07-20 23:00:00")}},
             {checkstatus: {$ne:"testok"}},
-//             {checkid:"20126304"},
-            {deviceid:563882}
-        ]
-    }        
-).limit(50)
+//             {description: {$search:"Anti"}},
+//             {checkid:"30073493"},
+            {description:{$not:/Anti/}},
+            {deviceid:1156225},
+         ],
+//         $not:
+//             description:{$nin:[/Anti/]}
+//             description:{$not:/Anti/}
+    },
+    {}    
+).limit(50).sort({datetime: 1})
