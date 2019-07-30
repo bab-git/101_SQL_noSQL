15a7e270941c2ebc803378d07924ffc51897c6df
Babak Hosseini <Babak Hosseini@zentrale.de>
2019-07-30 11:28:42 +0200

    continue

--- check_server_time.js
+++ check_server_time.js
@@ -2,18 +2,20 @@ db.getCollection('check').find(
     {
          $and: 
          [   
-//             {servertime: new ISODate("2019-07-20 12:02:34.000Z")},
-            {servertime: {"$gt": new ISODate("2019-07-01 00:00:00")}},
-            {servertime: {"$lt": new ISODate("2019-07-20 23:00:00")}},
-            {checkstatus: {$ne:"testok"}},
+//             {servertime: new ISODate("2019-06-01 01:04:55.000Z")},
+            {servertime: {"$gt": new ISODate("2019-07-26 01:00:55.000Z")}},
+            {servertime: {"$lt": new ISODate("2019-07-27 04:14:55.000Z")}},
+//             {checkstatus: {$ne:"testok"}}
 //             {description: {$search:"Anti"}},
-//             {checkid:"30073493"},
-            {description:{$not:/Anti/}},
+//             {checkid:"30073493"},
+//             {description:{$not:/Anti/}},
             {deviceid:1156225},
-         ],
-//         $not:
-//             description:{$nin:[/Anti/]}
+         ],
+//         $not:
+//             description:{$nin:[/Anti/]}
 //             description:{$not:/Anti/}
-    },
+    },
     {}    
-).limit(50).sort({datetime: 1})
+).limit(50)
+//     .sort({datetime: 1})
+    .sort({checkid: 1})    
