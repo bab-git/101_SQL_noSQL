dc27d83858e6b78acfbce22d4ca038a44817d4f4
Babak Hosseini <Babak Hosseini@zentrale.de>
2019-08-12 14:38:57 +0200

    fixing dsc247 == 2 issue

--- check_server_time.js
+++ check_server_time.js
@@ -4,26 +4,30 @@ db.getCollection('check').find(
 //          [   
 //             {servertime: new ISODate("2019-06-01 01:04:55.000Z")},
             servertime: {
-                            "$gte": new ISODate("2019-07-31 01:00:00.000Z"),
+                            "$gte": new ISODate("2019-06-06 01:00:00.000Z"),
                             "$lte": new ISODate("2019-07-31 23:59:59.000Z")
                           },
 //             {checkstatus: {$ne:"testok"}},
 //             {description: {$search:"Anti"}},
-//             {checkid:"29671730"},
+            checkid:"16880243",
 //             {description:{$not:/Anti/}},
-//             deviceid:1035046,
+            deviceid:745958,
 //             {deviceid:{"$gte":1035046}},
-               dsc247: {$nin:[1,2]}
+//                dsc247: {$nin:[1,2]}
 //          ],
 //         $not:
 //             description:{$nin:[/Anti/]}
 //             description:{$not:/Anti/}
-    }
-//     {
-//         deviceid:{$dsc247}
-//     }    
+    },
+    {
+        deviceid:1,
+        checkid:1,
+        checkstatus:1,
+        consecutiveFails:1,
+        servertime:1        
+    }    
 )
-.count()
+// .count()
 //     .limit(50)
-//     .sort({datetime: 1})
+    .sort({servertime: 1})
 //     .sort({checkid: 1})
