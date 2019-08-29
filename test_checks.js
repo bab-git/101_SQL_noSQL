9d06c8579dc8e869a56d5a754356e2907dc580b5
bab-git <bbkhosseini@gmail.com>
2019-08-29 11:12:31 +0200

    data extraction: fix last fail for one instances

--- test_checks.js
+++ test_checks.js
@@ -8,8 +8,8 @@ db.getCollection('check').find(
 //                 {datetime: { "$gt" : new ISODate("2019-02-04 01:00:10.000Z")}},
 //                 {datetime: { "$lt" : new ISODate("2019-07-01 01:00:17")}}
                servertime: { 
-                            "$gte" : ISODate("2019-06-01 06:00:00"),
-                            "$lt" : ISODate("2019-07-30 07:59:59")
+                            "$gte" : ISODate("2019-07-01 07:00:00"),
+                            "$lt" : ISODate("2019-07-01 09:59:59")
                            },
 //                {datetime: { "$lt" : new ISODate("2019-02-04 18:50:00")}},
 //                    {datetime: { "$gt" : new Date("2018-12-03")}},
@@ -19,10 +19,10 @@ db.getCollection('check').find(
 //                 {checkid: {$in: ["23538852","22261308"]}}
 //                {description : /Festplattenspei/},
 //                {checkstatus : "testerror"}
-//                deviceid: 1057500,
-               consecutiveFails: {"$gt":1}
-//                checkid: "26868037",
-//                checkstatus: {$ne:"testok"}
+//                deviceid: 1040947,
+               checkid: "11579026",
+//                checkstatus: {$ne:"testok"}
+//                   description : 'Skriptüberprüfung - Kinect Error'
 //             ]                         
         }     
 //     }
@@ -36,6 +36,6 @@ db.getCollection('check').find(
 //         }
 //         ,projection = { 'servertime': False }
     )
-//         .limit(50)
+        .limit(50)
 //         .count()
-        .sort({servertime: 1})
+        .sort({checkid: 1})
