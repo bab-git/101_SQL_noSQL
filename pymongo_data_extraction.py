e5fa9873f9aa07560df6e62701d9c62bdb95f175
bab-git <bbkhosseini@gmail.com>
2019-09-06 16:48:56 +0200

    pymongo modify check-list sheet

--- pymongo_data_extraction.py
+++ pymongo_data_extraction.py
@@ -364,8 +364,108 @@ if get_code != modif_code:
     raise ValueError('You cannot modify the spread sheet!!')
     
 
+load_file = 'check_extraction.sav'
+loaded_data = pickle.load( open(save_file, "rb" ))
+check_DB = loaded_data['check_DB']
 
+head_ind=8        # index of the header
+#label_col = 14
 
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Keys/mongoDB_secret.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("Checks list").sheet1
+#sheet2 = client.open("temp2").sheet1
+
+#fails_select = sheet.row_values(8)
+headers = sheet.row_values(head_ind)
+all_values = sheet.get_all_values()
+SQL_cpy = pd.DataFrame(all_values)
+SQL_cpy.to_excel('check_modify_copy.xlsx', index = False)
+
+#checks  = pd.DataFrame(all_values[head_ind:], columns = headers)
+sheet_checks  = pd.DataFrame(all_values, columns = headers)
+
+i_check = 0
+while i_check< len(sheet_checks):
+    extra = sheet_checks['extra'][i_check]
+    if extra == '':
+        i_check +=1
+        continue
+    checkname = sheet_checks['description'][i_check]
+    pr = H_annot(checkname,extra)
+    
+    if pr == 'ignore':
+        print (i_check)
+        raise ValueError('ignore')
+        continue    
+    
+    elif (pr == 'H') | (pr == 'nH'):
+        print (i_check)
+        raise ValueError('H or nH')
+        
+        g_row = sheet.row_values(i_check+1)
+        SQL_row = sheet_checks.iloc[i_check].values.tolist()
+        SQL_row = list(filter(lambda x: x != '', SQL_row))
+        
+        if g_row != SQL_row:
+            raise ValueError('SQL is not syncronized with google sheet \n update the SQL table from google-sheet')
+        
+        DB_col_list = check_DB.columns;
+        
+        
+        deviceid = int(sheet_checks.loc[i_check,'deviceid'])
+#        deviceid = 971448
+#        checkid = '30016492'
+        checkid = sheet_checks.loc[i_check,'checkid']
+        time =  datetime.strptime(sheet_checks.loc[i_check,'servertime'], '%Y-%m-%d %H:%M:%S')
+#        time = datetime(2019,7,5,15,43,16)
+        
+        dev_ind = check_DB['deviceid'] == deviceid
+        chk_ind = check_DB['checkid'] == checkid
+        tm_ind = check_DB['servertime'] > time
+        
+        if len(check_DB.loc[dev_ind].index) == 0:
+            ind_add = len(check_DB) # to the end of list
+        elif len(check_DB.loc[dev_ind & chk_ind].index) == 0:
+            ind_add = check_DB.loc[dev_ind].index[0]-1  # to the top of device list
+        elif len(check_DB.loc[dev_ind & chk_ind & tm_ind ].index) == 0:
+            ind_add = check_DB.loc[dev_ind & chk_ind].index[0]-1  # to the top of device-check list
+        else:            
+            ind_add = check_DB.loc[dev_ind & chk_ind & tm_ind ].index[0]-1  # after the device-check happened before this one
+            
+        #--------- update check_DB                
+        if ind_add == -1:
+            ind_add =0
+        check_DB1 = pd.DataFrame(check_DB.iloc[:ind_add+1])
+        check_DB2 = pd.DataFrame(columns = DB_col_list)
+        check_DB2.loc[0] = sheet_checks.loc[i_check,DB_col_list]
+        check_DB2['Label'] = pr
+        check_DB3 = pd.DataFrame(check_DB.iloc[ind_add+1:])
+        check_DB = pd.concat([check_DB1, check_DB2, check_DB3],  ignore_index=True)
+        
+
+        
+        #--------- update google sheet
+        i_rmv = i_check
+        if sheet_checks.loc[i_check+1,'deviceid'] == '':
+            i_rmv = [i_check+1,i_check]
+            for row in i_rmv:
+                sheet.delete_row(row+1)
+        else:
+            sheet.delete_row(i_rmv+1)
+                
+        #--------- update local sheet
+        sheet_checks = sheet_checks.drop(i_rmv).reset_index(drop = True)
+        
+        
+        
+        
+#        save_file = 'check_extraction.sav'
+        pickle.dump({'check_DB':check_DB}, open(load_file, 'wb'))
+        continue
+    
+    i_check +=1
 
 
 
@@ -396,10 +496,10 @@ results = checks.find(
 #                                    "$ne": "false"
                                     },    
 #                    "servertime": datetime(2019,3,1,18,51,21),
-                    "deviceid":1054972,
+                    "deviceid":1085384,
     #                "dsc247":2,
 #                    "checkstatus": {"$ne":"testok"},            
-                    "checkid": "26706789"
+                    "checkid": "27734051"
                 }
                 ,projection={'datetime': False}
                 )
--- temp.py
+++ temp.py
@@ -1,157 +1,87 @@
-check_DB=pd.DataFrame(columns = DB_col_list)
-#%%
-#while i < len(device_db):
-#while i <= len(device_db):shab mi
-#    i = 0
-    #device_id = WK_list[i]['_id']    
-    #  %%
-    device_id = int(device_db['_id'][i])
-    print('\nGetting checks for device_id:',i,'/',len(device_db),'(%s)' % (device_db['device_name'][i]),'...')
-#    device_id=int(device_db['_id'][device_db['device_name']=='SRV-PR-01'])
-    device_id = 960448
-    i = device_db.loc[device_db['_id']==1054972,'_id'].index[0]        
-#    del resultsd
-    results = checks.find(
-                {
-                    "servertime":
-                    {
-                                    "$gte": datetime(2019,6,1,0,0,0),
-                                    "$lte": datetime(2019,7,31,23,59,59)
-                                    },    
-                    "deviceid":device_id,
-#                    "deviceid":1054972,
-    #                "dsc247":2, 
-                    "checkstatus": {"$ne":"testok"},
-#                    "checkid": "26706789"
-                }
-                ,projection={'datetime': False}
-                )
-#    try:
-    check_results = list(results)
-#    except pymongo.errors.InvalidBSON:
-#       print('year format prblem ==> skip the device')
-#       i+=1
-#       year_prob.append(device_id)
-#       break
-    #  %%
-    len_fails = len(check_results)
-    print('number of failed checks:',len_fails)
-#  %%
-#    if len(check_results) == 0:
-#        i+=1
-#        continue
-    print('Prepaing the SQL table...')
-    check_SQL=pd.DataFrame(check_results, columns = ['servertime','description',
-                                                     'checkstatus','consecutiveFails','dsc247',
-                                                     'extra','checkid','deviceid']).sort_values(by = 
-#                                                        'servertime')#, ascending = False)
-                                                    ['checkid','servertime'])#, ascending = False)                              
-    temp = device_db.loc[i,['device_name','client_name','site_name','Type']]
-    check_SQL[['device_name','client_name','site_name','Type']] = check_SQL.apply(lambda row: temp, axis = 1)      
-    check_SQL = check_SQL.reset_index(drop = True)
-    check_SQL['consFails'] = ''
-    check_SQL['last_fail'] = ''
-    check_SQL0 = check_SQL.copy()  # for debuging - todo remove
-    
-    check_current = check_SQL.loc[0,'checkid']
-    check_SQL.loc[0,'consFails'] = check_SQL.loc[0,'consecutiveFails']
-    check_SQL.loc[0,'last_fail'] = check_SQL.loc[0,'servertime']
-    dsc247 = check_SQL['dsc247'][0]
-   
-    i_f = 1
- #  %%   loop over the check-fail rows
-    while i_f < len(check_SQL):
-#            if check_SQL['servertime'][i_f] >= datetime(2019,6,3,7,10,0):
-#                break   
-#  %%                 
-        check_next = check_SQL.loc[i_f,'checkid']
-#        if check_next == '26706789':
-#            break        
-        checkname = check_SQL.loc[i_f,'description']
-        extra = check_SQL.loc[i_f,'extra']
-        if H_annot(checkname,extra)=='ignore': # check-definite label 3 case
-            check_SQL = check_SQL.drop(i_f).reset_index(drop = True)
-            #                print('ignore')
-#            print('continue')
-            continue
-                
-        if check_next == check_current:  # same check
-            seq = 0  # flags the existing sequence
+#import pandas as pd
+#ind_add : the row under which the entry should be added!
 
-            a = check_SQL['last_fail'][i_f-1]
-            b = check_SQL['servertime'][i_f]
-            cons_b = check_SQL['consecutiveFails'][i_f]
-            cons_a = check_SQL['consecutiveFails'][i_f-1]
-            
-            if ((b - a).total_seconds() < 3.5*3600): # 2-3hr consequative errors
-                seq = 1
-            elif (b.day-a.day == 1 or (b.day-a.day <0 and (b-a).total_seconds() < 24*3600) ): # next day sequence
-                if dsc247 == 2 : # safety check or consecutiveFails
-                    seq = 1
-                else:    # look for in between testok!
-                    
-                    results = checks.find(
-                                        {
-                                            "servertime": {
-                                                            "$gte": a,
-                                                            "$lte": b
-                                                            },    
-                                            "deviceid":device_id,                                
-                                            "checkstatus": "testok",
-                                            "checkid": check_current                                            }
-                                        )
-                    temp_results = list(results)
-                    if len(temp_results) == 0: # continues fail sequence
-                        seq = 1
-            if seq == 1:  # continues failing sequanece ==> clear it from the table    
-                check_SQL.loc[i_f-1,'extra'] = check_SQL.loc[i_f,'extra']
-                check_SQL = check_SQL.drop(i_f).reset_index(drop = True)
-                i_f -= 1
-                
-            check_SQL.loc[i_f,'last_fail'] = b
-            check_SQL.loc[i_f,'consFails'] = cons_b
-        else:  # new check            
-            # adding new row
-#                break
-#                print('break')
-            dsc247 = check_SQL['dsc247'][i_f]
-            check_SQL.loc[i_f,'last_fail'] = check_SQL.loc[i_f,'servertime']
-        
-        if (check_next != check_current) | (seq == 0):
-            # categorizing previous row#                
-            checkname = check_SQL.loc[i_f-1,'description']
-            extra = check_SQL.loc[i_f-1,'extra']
-            pr = H_annot(checkname,extra)
-            if pr == 'ignore':
-                check_SQL = check_SQL.drop(i_f-1).reset_index(drop = True)
-                i_f -= 1
-#                    print('ignore')
-            elif (pr=='nH')|(pr=='H'): # check-definite label H/N case
-#                    break
-                temp = check_SQL.loc[i_f-1,DB_col_list[:len(DB_col_list)-1]]
-                temp['consecutiveFails'] = check_SQL.loc[i_f-1,'consFails']
-                temp['Label'] = pr
-                check_DB= check_DB.append(temp,ignore_index=True)
-                check_SQL = check_SQL.drop(i_f-1).reset_index(drop = True)
-                i_f -= 1
-#                    print('Add')                    
-#                    break                                        
-                
-        if check_SQL['consFails'][i_f]=='':
-                check_SQL.loc[i_f,'consFails'] = check_SQL.loc[i_f,'consecutiveFails']
-        check_current = check_next                
-        i_f += 1
-# %%
-    # Annotate last entry of SQL table
-    i_f = len(check_SQL)-1
-    checkname = check_SQL.loc[i_f,'description']
-    extra = check_SQL.loc[i_f,'extra']
+i_check = 0
+while i_check< len(sheet_checks):
+    extra = sheet_checks['extra'][i_check]
+    if extra == '':
+        i_check +=1
+        continue
+    checkname = sheet_checks['description'][i_check]
     pr = H_annot(checkname,extra)
-    if pr == 'ignore': # check-definite label 3 case
-        check_SQL = check_SQL.drop(i_f).reset_index(drop = True)
-    elif (pr=='nH')|(pr=='H'): # check-definite label H/N case
-        temp = check_SQL.loc[i_f,DB_col_list[:len(DB_col_list)-1]]
-        temp['consecutiveFails'] = check_SQL.loc[i_f,'consFails']
-        temp['Label'] = pr
-        check_DB= check_DB.append(temp,ignore_index=True)     
-        check_SQL = check_SQL.drop(i_f).reset_index(drop = True)
+    
+    if pr == 'ignore':
+        print (i_check)
+        raise ValueError('ignore')
+        continue    
+    
+    elif (pr == 'H') | (pr == 'nH'):
+        print (i_check)
+        raise ValueError('H or nH')
+        
+        g_row = sheet.row_values(i_check+1)
+        SQL_row = sheet_checks.iloc[i_check].values.tolist()
+        SQL_row = list(filter(lambda x: x != '', SQL_row))
+        
+        if g_row != SQL_row:
+            raise ValueError('SQL is not syncronized with google sheet \n update the SQL table from google-sheet')
+        
+        DB_col_list = check_DB.columns;
+        
+        
+        deviceid = int(sheet_checks.loc[i_check,'deviceid'])
+#        deviceid = 971448
+#        checkid = '30016492'
+        checkid = sheet_checks.loc[i_check,'checkid']
+        time =  datetime.strptime(sheet_checks.loc[i_check,'servertime'], '%Y-%m-%d %H:%M:%S')
+#        time = datetime(2019,7,5,15,43,16)
+        
+        dev_ind = check_DB['deviceid'] == deviceid
+        chk_ind = check_DB['checkid'] == checkid
+        tm_ind = check_DB['servertime'] > time
+        
+        if len(check_DB.loc[dev_ind].index) == 0:
+            ind_add = len(check_DB) # to the end of list
+        elif len(check_DB.loc[dev_ind & chk_ind].index) == 0:
+            ind_add = check_DB.loc[dev_ind].index[0]-1  # to the top of device list
+        elif len(check_DB.loc[dev_ind & chk_ind & tm_ind ].index) == 0:
+            ind_add = check_DB.loc[dev_ind & chk_ind].index[0]-1  # to the top of device-check list
+        else:            
+            ind_add = check_DB.loc[dev_ind & chk_ind & tm_ind ].index[0]-1  # after the device-check happened before this one
+
+
+        !!! HERE!!  check_DB imports lose their type!!!
+
+               
+        #--------- update check_DB                
+        if ind_add == -1:
+            ind_add =0
+        check_DB1 = pd.DataFrame(check_DB.iloc[:ind_add+1])
+        check_DB2 = pd.DataFrame(columns = DB_col_list)
+        check_DB2.loc[0] = sheet_checks.loc[i_check,DB_col_list]
+        check_DB2['Label'] = pr
+        check_DB3 = pd.DataFrame(check_DB.iloc[ind_add+1:])
+        check_DB = pd.concat([check_DB1, check_DB2, check_DB3],  ignore_index=True)
+        
+
+        
+        #--------- update google sheet
+        i_rmv = i_check
+        if sheet_checks.loc[i_check+1,'deviceid'] == '':
+            i_rmv = [i_check+1,i_check]
+            for row in i_rmv:
+                sheet.delete_row(row+1)
+        else:
+            sheet.delete_row(i_rmv+1)
+                
+        #--------- update local sheet
+        sheet_checks = sheet_checks.drop(i_rmv).reset_index(drop = True)
+        
+        
+        
+        
+#        save_file = 'check_extraction.sav'
+        pickle.dump({'check_DB':check_DB}, open(load_file, 'wb'))
+        continue
+    
+    i_check +=1
